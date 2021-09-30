5f6db2d1af1985000f30761f38623dcd84eb3c23
Evgeniy <90176476+NaymushinEvgeniy@users.noreply.github.com>
2021-09-30 11:23:12 +0300

    Create...

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+a = 15;
+console.log(a);
+
+let number = 5;
+const leftBorderWidht = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a:50
+};
+
+obj.a =10;
+console.log(obj)
+
+console.log(name);
+var name = 'Ivan';
+
