69742636fa858b028d188f3cfd3ee3e1c766e592
Evgeniy Work <zheka0072007@rambler.ru>
2021-10-01 14:40:23 +0300

    exec13

--- 024 script.js
+++ 024 script.js
@@ -70,7 +70,7 @@ const personalMovieDB = {
         for( let i=0; i < 3; i++) {
             personalMovieDB.genres[i] = prompt(`Ваш любимы жанр под номером ${i+1}` );
             if (personalMovieDB.genres[i]==null || personalMovieDB.genres[i]=='') {
-                i = -1;
+                i--;
             } 
         }
     },
